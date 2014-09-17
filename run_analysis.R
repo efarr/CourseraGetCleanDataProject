af49cb508dd8beb1f8f3c38ffeca00264ad443f6
efarr <eric@farrnet.com>
2014-09-17 17:28:14 -0400

    Read in features and activities.

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,36 @@
 ## Coursera Getting and Cleaning Data Class Project
 
+## Download and unzip the file
+
+## Read feature names
+features <- read.table("./OriginalData/features.txt")
+colnames(features) <- c("number", "name")
+
+## Read activity labels
+activities <- read.table("./OriginalData/activity_labels.txt")
+colnames(activities) <- c("number", "name")
+
+## Only columns with -mean or -std in the name.
+desiredColumns <- subset(features, grepl("mean()", name, fixed=TRUE) | grepl("std()", name))
+
 ## Merge the training and the test sets to create one data set.
-testSubject <- read.table("./OriginalData/test/subject_test.txt")
-testX <- read.table("./OriginalData/test/X_test.txt")
-testY <- read.table("./OriginalData/test/Y_test.txt")
-print(nrow(testSubject))
-print(nrow(testX))
-print(nrow(testY))
+mergeFiles <- function(subjectFilename, xFilename, yFilename) {
+    subjectFile <- read.table(subjectFileName)
+    xFile <- read.table(xFileName)
+    yFile <- read.table(yFileName)
+    
+    if (nrow(subjectFile) != nrow(xFile) | nrow(xFile) != nrow(yFile)) {
+        stop("input files have differen row counts")
+    }
+    
+    allCols <- cbind(subjectFile, yFile, xFile)
+}
+
+testFile <- mergeFiles( "./OriginalData/test/subject_test.txt",
+                        "./OriginalData/test/X_test.txt",
+                        "./OriginalData/test/Y_test.txt")
+
+print(nrow(testFile))
 
 ## Extract only the measurements on the mean and standard deviation for each measurement. 
 ## Uses descriptive activity names to name the activities in the data set
