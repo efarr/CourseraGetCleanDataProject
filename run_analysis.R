cae7564c032f2d551b3880ee3b5e96d691e836bf
efarr <eric@farrnet.com>
2014-09-19 17:09:38 -0400

    Closer.

--- run_analysis.R
+++ run_analysis.R
@@ -3,47 +3,53 @@
 library(dplyr)
 library(tidyr)
 
-## Download and unzip the file
-
 ## Read feature names
 features <- read.table("./OriginalData/features.txt")
 colnames(features) <- c("number", "name")
+features$name <- as.character(features$name)
 
 ## Read activity labels
 activities <- read.table("./OriginalData/activity_labels.txt")
-colnames(activities) <- c("number", "name")
+colnames(activities) <- c("number", "Activity")
 
 ## Only columns with -mean or -std in the name.
 desiredColumns <- subset(features, grepl("mean()", name, fixed=TRUE) | grepl("std()", name))
 
-## Merge the training and the test sets to create one data set.
-mergeFiles <- function(subjectFilename, xFilename, yFilename) {
-    subjectFile <- read.table(subjectFileName)
-    xFile <- read.table(xFileName)
-    yFile <- read.table(yFileName)
-    
-    if (nrow(subjectFile) != nrow(xFile) | nrow(xFile) != nrow(yFile)) {
-        stop("input files have different row counts")
-    }
-    
-    
-    
-    allCols <- tbl_df(cbind(subjectFile, yFile, xFile))
+## Clean up the column names
+desiredColumns$name <- gsub("\\(|\\)", "", desiredColumns$name)
+desiredColumns$name <- gsub("-", "_", desiredColumns$name)
+desiredColumns$name <- gsub("^t", "", desiredColumns$name)
+desiredColumns$name <- gsub("^f", "Freq_", desiredColumns$name)
+
+## Combine test and train sets
+trainRaw <- read.table("./OriginalData/train/X_train.txt")
+testRaw <- read.table("./OriginalData/test/X_test.txt")
+
+if (ncol(trainRaw) != ncol(testRaw)) {
+    stop("input files have different column counts")
 }
 
-testFile <- mergeFiles( "./OriginalData/test/subject_test.txt",
-                        "./OriginalData/test/X_test.txt",
-                        "./OriginalData/test/Y_test.txt")
+values <- tbl_df(rbind(trainRaw, testRaw))
 
-trainFile <- mergeFiles( "./OriginalData/train/subject_test.txt",
-                         "./OriginalData/train/X_test.txt",
-                         "./OriginalData/train/Y_test.txt")
+## Select just std and mean columns
+values <- values[,desiredColumns$number]
+colnames(values) <- desiredColumns$name
 
-## Extract only the measurements on the mean and standard deviation for each measurement. 
-## Get just the mean & std columns with select()
+## Combine test and train activity labels
+trainRaw <- read.table("./OriginalData/train/Y_train.txt")
+testRaw <- read.table("./OriginalData/test/Y_test.txt")
 
+activity <- tbl_df(rbind(trainRaw, testRaw))
 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Add descriptive labels
+colnames(activity) <- "number"
+activity <- left_join(activity, activities, by="number")
+activity <- select(activity,2)
 
+## Combine test and train subjects
+trainRaw <- read.table("./OriginalData/train/subject_train.txt")
+testRaw <- read.table("./OriginalData/test/subject_test.txt")
+subject <- tbl_df(rbind(trainRaw, testRaw))
+colnames(subject) <- "Subject"
+
+tidy <- tbl_df(cbind(subject, activity, values))
