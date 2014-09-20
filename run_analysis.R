0ca7d4825aaa8003878a528fce845526d7d581d5
Eric Farr <eric@farrnet.com>
2014-09-20 09:20:05 -0400

    Successfully produced tidy file.

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,28 @@
 ## Coursera Getting and Cleaning Data Class Project
-
 library(dplyr)
-library(tidyr)
+
+dirName <- "./OriginalData"
+
+trainFile <- paste(dirName, "train/X_train.txt", sep="/")
+testFile <- paste(dirName, "test/X_test.txt", sep="/")
+trainActFile <- paste(dirName, "train/Y_train.txt", sep="/")
+testActFile <- paste(dirName, "test/Y_test.txt", sep="/")
+trainSbjFile <- paste(dirName, "train/subject_train.txt", sep="/")
+testSbjFile <- paste(dirName, "test/subject_test.txt", sep="/")
+activityNameFile <- paste(dirName, "activity_labels.txt", sep="/")
+featureFile <- paste(dirName, "features.txt", sep="/")
 
 ## Read feature names
-features <- read.table("./OriginalData/features.txt")
+features <- read.table(featureFile)
 colnames(features) <- c("number", "name")
-features$name <- as.character(features$name)
 
 ## Read activity labels
-activities <- read.table("./OriginalData/activity_labels.txt")
+activities <- read.table(activityNameFile)
 colnames(activities) <- c("number", "Activity")
 
 ## Only columns with -mean or -std in the name.
-desiredColumns <- subset(features, grepl("mean()", name, fixed=TRUE) | grepl("std()", name))
+desiredColumns <- subset(features, 
+                         grepl("mean()", name, fixed=TRUE) | grepl("std()", name))
 
 ## Clean up the column names
 desiredColumns$name <- gsub("\\(|\\)", "", desiredColumns$name)
@@ -22,34 +31,28 @@ desiredColumns$name <- gsub("^t", "", desiredColumns$name)
 desiredColumns$name <- gsub("^f", "Freq_", desiredColumns$name)
 
 ## Combine test and train sets
-trainRaw <- read.table("./OriginalData/train/X_train.txt")
-testRaw <- read.table("./OriginalData/test/X_test.txt")
-
-if (ncol(trainRaw) != ncol(testRaw)) {
-    stop("input files have different column counts")
-}
-
-values <- tbl_df(rbind(trainRaw, testRaw))
+values <- rbind(read.table(trainFile), read.table(testFile))
 
 ## Select just std and mean columns
 values <- values[,desiredColumns$number]
 colnames(values) <- desiredColumns$name
 
-## Combine test and train activity labels
-trainRaw <- read.table("./OriginalData/train/Y_train.txt")
-testRaw <- read.table("./OriginalData/test/Y_test.txt")
+## Combine test and train activity types (numbered)
+activityByNumber <- rbind(read.table(trainActFile), read.table(testActFile))
 
-activity <- tbl_df(rbind(trainRaw, testRaw))
-
-## Add descriptive labels
-colnames(activity) <- "number"
-activity <- left_join(activity, activities, by="number")
-activity <- select(activity,2)
+## Convert to descriptive labels
+colnames(activityByNumber) <- "number" #Common column name to join on
+activity <- left_join(activityByNumber, activities, by="number", by.y=1) %.% 
+    select(2)
 
 ## Combine test and train subjects
-trainRaw <- read.table("./OriginalData/train/subject_train.txt")
-testRaw <- read.table("./OriginalData/test/subject_test.txt")
-subject <- tbl_df(rbind(trainRaw, testRaw))
+subject <- rbind(read.table(trainSbjFile), read.table(testSbjFile))
 colnames(subject) <- "Subject"
 
-tidy <- tbl_df(cbind(subject, activity, values))
+## Aggregate data into mean of feature value by subject and activity
+final <- cbind(subject, activity, values) %.%
+    group_by(Subject,Activity) %.%
+    summarise_each(funs(mean))
+
+#Write final, tidy file
+write.csv(final, "means_by_subj_and_activivity.txt", row.names=FALSE)
